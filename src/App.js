d5e09d60033fa0328f227996bd35a88e10bb593f
vicfir <vicfir@hotmail.fr>
2023-02-24 17:57:03 +0100

    score components ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import { Choice } from "./components/choice";
 import { Score } from "./components/score";
 
 function App() {
+  const [score, setScore] = useState(0);
+
   return (
     <div className="App">
-        <Score/>
+        <Score score={score}/>
         <Choice/>
     </div>
   );
--- src/components/score.js
+++ src/components/score.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-export const Score = () => {
+export const Score = (props) => {
   return (
-    <div className='flex justify-between max-w-3xl mx-auto'>
-        <div className=''>
+    <div className='flex justify-between max-w-3xl mx-auto border-2 border-slate-500 rounded-xl p-5 '>
+        <div className='font-bold text-3xl leading-none text-white'>
             <h1>ROCK</h1>
             <h1>PAPER</h1>
             <h1>SCISSORS</h1>
         </div>
-        <div>
-            <p>SCORE</p>
-            <p>12</p>
+        <div className='bg-white px-9 rounded-lg flex flex-col justify-center items-center leading-none'>
+            <p className='text-sm tracking-widest text-indigo-700 font-semibold'>SCORE</p>
+            <p className='font-bold text-5xl text-slate-600'>{props.score}</p>
         </div>
     </div>
   )
