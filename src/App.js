3f372dd1d3c3bdf6078081b1b76395c195c83936
vicfir <vicfir@hotmail.fr>
2023-02-24 21:29:09 +0100

    rules modal style + script

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import { useState } from 'react';
 import './App.scss';
 import { Choice } from "./components/choice";
 import { Score } from "./components/score";
+import { Rules } from "./components/rules";
 
 function App() {
   const [score, setScore] = useState(0);
+
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <div className="App">
         <Score score={score}/>
         <Choice/>
+        <button
+          className='text-white border rounded-lg px-8 py-2 tracking-widest absolute bottom-10 right-10' 
+          type="button"
+          onClick={() => setShowModal(true)}
+        >
+          RULES
+        </button>
+        {showModal ? <Rules modal={setShowModal}/> : null}
     </div>
   );
 }
